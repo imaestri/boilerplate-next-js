df914d1ec78cb66dd6acd1a811de0225a08ed342
imaestri <isaquemaestri01@gmail.com>
2021-07-07 21:56:40 -0300

    criando estilos globais e do component main

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,18 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e react avançado escrito ao lado"
+    />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>Typescript, ReactJs e styled Components</S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="desenvolvedor criando código"
+    />
+  </S.Wrapper>
 )
 
 export default Main
